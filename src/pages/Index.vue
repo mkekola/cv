32f53956371e74def2ee16085cc45add6f059f24
Maria <maria.kekola@helsinki.fi>
2022-10-15 01:17:46 +0300

    lisätty flexbox hahmotelma

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,33 @@
 <template>
   <q-page>
-    <div class="q-pa-md">
-      <div class="q-col-gutter-md row items-start">
+    <div class="container">
+      <div class="top-row">
+        <div class="col-1">
+          <div class="profile-img">
+            <q-img style="height: 600px; width: 45em;" :fit='contain' src="../assets/cv_kuva.jpg" alt=""/>
+          </div>
+        </div>
+        <div class="col-2">
+          Second column
+        </div>
+      </div>
+      <div class="middle-row">
+        <div class="col-3">
+          First column
+        </div>
+        <div class="col-4">
+          Second column
+        </div>
+      </div>
+      <div class="bottom-row">
+        <div class="col-5">
+          First column
+        </div>
         <div class="col-6">
-          <q-img src="../assets/cv_kuva.jpg" no-native-menu>
-            <div class="absolute-bottom text-subtitle1 text-left">
-              Työkokemus
-            </div>
-          </q-img>
+          Second column
         </div>
       </div>
     </div>
-
   </q-page>
 </template>
 
@@ -31,8 +47,72 @@ export default class PageIndex extends Vue {
 
 <style>
 
-.q-pa-md q-img{
-  height: 550px;
+.container {
+  background: rgba(15,15,15,0.95);
+}
+
+q-img {
+  margin: auto;
+  width: 100%;
+  max-width: 45em;
+  max-height: 100%;
+}
+
+.top-row {
+  display: flex;
+  justify-content: center;
+}
+
+.middle-row {
+  justify-content: center;
+  display: flex;
+}
+
+.bottom-row {
+  justify-content: center;
+  display: flex;
+}
+
+.col-1 {
+  background: #121212;
+  height: 600px;
+  width: 45em;
+  object-fit: contain;
+}
+
+.col-2 {
+  background: #ffffff;
+  padding: 15px;
+  height: 600px;
+  width: 45em;
+}
+
+.col-3 {
+  background: #223444;
+  padding: 15px;
+  height: 600px;
+  width: 45em;
+}
+
+.col-4 {
+  background: #ffffff;
+  padding: 15px;
+  height: 600px;
+  width: 45em;
+}
+
+.col-5 {
+  background: #223444;
+  padding: 15px;
+  height: 400px;
+  width: 45em;
+}
+
+.col-6 {
+  background: #ffffff;
+  padding: 15px;
+  height: 400px;
+  width: 45em;
 }
 
 </style>
